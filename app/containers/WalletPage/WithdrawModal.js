eb1564e1ff6af9c5397f846d954d186b9d19c45e
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-10-27 05:31:56 +0700

    withdraw modal: empty state

--- app/containers/WalletPage/WithdrawModal.js
+++ app/containers/WalletPage/WithdrawModal.js
@@ -8,7 +8,16 @@ import type { Map } from 'immutable';
 import { createStructuredSelector } from 'reselect';
 import { withStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import { makeSelectWithdrawModal } from './selectors';
+
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import CloudOff from '@material-ui/icons/CloudOff';
+
+import {
+  makeSelectWithdrawModal,
+  makeSelectCoinWithdrawModal
+} from './selectors';
 import { closeWithdrawModal } from './actions';
 
 const debug = require('debug')('dicoapp:containers:WalletPage:WithdrawModal');
@@ -19,23 +28,64 @@ type Props = {
   // eslint-disable-next-line flowtype/no-weak-types
   onClose: Function,
   // eslint-disable-next-line flowtype/no-weak-types
-  withdrawModal: Map<*, *>
+  withdrawModal: Map<*, *>,
+  // eslint-disable-next-line flowtype/no-weak-types
+  coin: Map<*, *> | null
 };
 
-const styles = () => ({});
+const styles = () => ({
+  withdrawmodal__emptystate: {
+    position: 'absolute',
+    top: '45%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: 25
+  },
+
+  withdrawmodal__iconemptystate: {
+    fontSize: 50
+  },
+
+  withdrawmodal__content: {
+    width: 500,
+    textAlign: 'center'
+  }
+});
 
 export class WithdrawModal extends React.PureComponent<Props> {
+  renderEmptyState = () => {
+    const { classes } = this.props;
+    return (
+      <div className={classes.withdrawmodal__emptystate}>
+        <Typography variant="title" gutterBottom>
+          <CloudOff className={classes.withdrawmodal__iconemptystate} />
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          No data found. Please select an asset.
+        </Typography>
+      </div>
+    );
+  };
+
+  renderCoin = () => <React.Fragment>renderCoin</React.Fragment>;
+
   render() {
     debug('render');
-    const { classes, withdrawModal, onClose } = this.props;
+    const { classes, withdrawModal, coin, onClose } = this.props;
     return (
       <SwipeableDrawer
         anchor="right"
         open={withdrawModal.get('open')}
         onClose={onClose}
       >
-        <div tabIndex={0} role="button" className={classes.swapDetail__content}>
-          WithdrawModal
+        <div
+          tabIndex={0}
+          role="button"
+          className={classes.withdrawmodal__content}
+        >
+          {!coin && this.renderEmptyState()}
+          {coin && this.renderCoin()}
+          {this.renderEmptyState()}
         </div>
       </SwipeableDrawer>
     );
@@ -50,7 +100,8 @@ export function mapDispatchToProps(dispatch: Dispatch<Object>) {
 }
 
 const mapStateToProps = createStructuredSelector({
-  withdrawModal: makeSelectWithdrawModal()
+  withdrawModal: makeSelectWithdrawModal(),
+  coin: makeSelectCoinWithdrawModal()
 });
 
 const withConnect = connect(
