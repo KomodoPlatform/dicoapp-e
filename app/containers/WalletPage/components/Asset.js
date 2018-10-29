321938549e6ebdfdf1722b3812030660bf16fe58
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-10-29 15:24:48 +0700

    [started #68] [skip ci] implement-portfolio-tab https://github.com/KomodoPlatform/dicoapp-e/issues/68

--- app/containers/WalletPage/components/Asset.js
+++ app/containers/WalletPage/components/Asset.js
@@ -67,12 +67,12 @@ const styles = theme => ({
 
   wallet__card: {
     border: '1px solid #dadce0',
-    boxShadow: 'none',
-    '&:hover': {
-      boxShadow:
-        '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
-      cursor: 'pointer'
-    }
+    boxShadow: 'none'
+    // '&:hover': {
+    //   boxShadow:
+    //     '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
+    //   cursor: 'pointer'
+    // }
   }
 });
 
