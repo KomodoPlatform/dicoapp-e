4f6451a114d7eb136d602955466fa5c1d04ac5b6
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-11-07 15:29:57 +0700

    jest: update jest config

--- app/main.dev.js
+++ app/main.dev.js
@@ -43,7 +43,7 @@ const installExtensions = async () => {
   const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
   return Promise.all(
     extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch(console.log);
+  ).catch(debug);
 };
 
 /**
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: true,
+  testURL: 'http://localhost/',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/internals/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    'electron-util': '<rootDir>/__mocks__/electron-util.js',
+    config$: '<rootDir>/__mocks__/config.js'
+  },
+  moduleFileExtensions: ['js'],
+  moduleDirectories: ['node_modules', 'app/node_modules'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  setupFiles: ['./internals/scripts/CheckBuiltsExist.js'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html']
+};
